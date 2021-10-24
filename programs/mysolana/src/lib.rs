1366d748fe18fcc1885d4522043a292c61fa595a
Aoi <ht.so.ik0102@gmail.com>
2021-10-24 09:37:24 +0700

    update lib.rs

--- programs/mysolana/src/lib.rs
+++ programs/mysolana/src/lib.rs
@@ -6,40 +6,37 @@ declare_id!("4B2EcqWMuDwGjy1aFRMEKrrf7JakJHiJHyeKwv12GZxn");
 pub mod mysolana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        let copy = data.clone();
-        base_account.data = data;
-        base_account.data_list.push(copy);
+        base_account.count = 0;
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        let copy = data.clone();
-        base_account.data = data;
-        base_account.data_list.push(copy);
+        base_account.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 64 + 64)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub system_program: Program <'info, System>,
 }
 
+// Transaction instructions
 #[derive(Accounts)]
-pub struct Update<'info> {
+pub struct Increment<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
 }
 
+// An account that goes inside a transaction instruction
 #[account]
 pub struct BaseAccount {
-    pub data: String,
-    pub data_list: Vec<String>,
+    pub count: u64,
 }
