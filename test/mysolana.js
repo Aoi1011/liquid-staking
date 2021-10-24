8874d64504604d2f56389b3db7380995e234f12c
Aoi <ht.so.ik0102@gmail.com>
2021-10-24 09:39:27 +0700

    update test case

--- test/mysolana.js
+++ test/mysolana.js
@@ -2,13 +2,15 @@ const assert = require("assert");
 const anchor = require("@project-serum/anchor");
 const { SystemProgram } = anchor.web3;
 
-describe("Mysolanaapp", () => {
+describe("mysolanaapp", () => {
+  /* create and set a Provider */
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.Mysolanaapp;
-  it("It initializes the account", async () => {
+  it("Creates a counter)", async () => {
+    /* Call the create function via RPC */
     const baseAccount = anchor.web3.Keypair.generate();
-    await program.rpc.initialize("Hello World", {
+    await program.rpc.create({
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
@@ -17,27 +19,25 @@ describe("Mysolanaapp", () => {
       signers: [baseAccount],
     });
 
+    /* Fetch the account and check the value of count */
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('Data: ', account.data);
-    assert.ok(account.data === "Hello World");
+    console.log('Count 0: ', account.count.toString())
+    assert.ok(account.count.toString() == 0);
     _baseAccount = baseAccount;
 
   });
 
-  it("Updates a previously created account", async () => {
+  it("Increments the counter", async () => {
     const baseAccount = _baseAccount;
 
-    await program.rpc.update("Some new data", {
+    await program.rpc.increment({
       accounts: {
         baseAccount: baseAccount.publicKey,
       },
     });
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('Updated data: ', account.data)
-    assert.ok(account.data === "Some new data");
-    console.log('all account data:', account)
-    console.log('All data: ', account.dataList);
-    assert.ok(account.dataList.length === 2);
+    console.log('Count 1: ', account.count.toString())
+    assert.ok(account.count.toString() == 1);
   });
 });
